53aa88ed5efd64c4fbbb45910a0c36cd882149c6
JeffersonAddai-Poku <jeffepok@gmail.com>
2022-02-24 02:20:57 +0000

    add social media icons

--- src/modules/site/components/navbar/navbar.tsx
+++ src/modules/site/components/navbar/navbar.tsx
@@ -1,15 +1,15 @@
 import React,{ Fragment } from 'react';
 import MenuItem from "../menuItem/menuItem";
 import {Menu, Transition} from '@headlessui/react'
-import {BsChevronDown, BsHouseFill} from "react-icons/bs"
+import {BsChevronDown, BsHouseFill, BsGithub, BsTwitter, BsFacebook} from "react-icons/bs"
 import Typist from "react-typist";
-import logo from "../../../../assets/images/logo.svg"
+import logo from "../../../../assets/images/logo-white.svg"
 
 const Navbar = () => {
     return (
         <div className="mb-11">
             <nav
-                className="z-20 sticky top-0 relative px-2 flex justify-between bg-gray-300 mb-3">
+                className="z-20 sticky top-0 relative px-2 flex items-center justify-between bg-gray-300 mb-3">
                 <div className="flex">
                     <div
                         className="rounded-t-md relative bg-white mt-1 pl-3 pr-14">
@@ -85,17 +85,19 @@ const Navbar = () => {
                     </Menu>
 
                 </div>
-                <a href="/"><img className={"w-10 p-1"} src={logo} alt="logo"/></a>
-                <div>Social media logos</div>
+                <div className="flex">
+                    <a className="self-center mr-2" href="https://github.com/jeffepok" target="_blank"><BsGithub/></a>
+                    <a className="self-center mr-2" href="https://twitter.com/Tee4Jey" target="_blank"><BsTwitter/></a>
+                    <a className="self-center mr-2" href="https://web.facebook.com/addaipokut" target="_blank"><BsFacebook/></a>
+                </div>
             </nav>
             <div className="text-white">
-                <Typist>
+                <a href="/"><img className={"w-14 mx-auto p-1 mb-5"} src={logo} alt="logo"/></a>
+                <hr/>
+                <Typist className="mt-5">
                     <Typist.Delay ms={1000} />
                     JEFFERSON TUFFOUR
                 </Typist>
-                <p>
-                    Full-Stack Developer
-                </p>
             </div>
         </div>
     )
