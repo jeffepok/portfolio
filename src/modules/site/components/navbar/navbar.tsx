2323daf73702890850f974e19e24d957a2f72a57
JeffersonAddai-Poku <jeffepok@gmail.com>
2022-03-04 20:58:42 +0000

    take logo to navbar

--- src/modules/site/components/navbar/navbar.tsx
+++ src/modules/site/components/navbar/navbar.tsx
@@ -1,22 +1,22 @@
-import React,{ Fragment } from 'react';
-import MenuItem from "../menuItem/menuItem";
-import {Menu, Transition} from '@headlessui/react'
-import {BsChevronDown, BsHouseFill, BsGithub, BsTwitter, BsFacebook} from "react-icons/bs"
-import Typist from "react-typist";
-import logo from "../../../../assets/images/logo-white.svg"
+import React, { Fragment } from 'react'
+import MenuItem from "../menuItem/menuItem"
+import { Menu, Transition } from '@headlessui/react'
+import { BsChevronDown, BsHouseFill, BsGithub, BsTwitter, BsFacebook } from "react-icons/bs"
+import Typist from "react-typist"
+import logo from "../../../../assets/images/logo.svg"
 
 const Navbar = () => {
     return (
         <div className="mb-11">
             <nav
-                className="z-20 sticky top-0 relative px-2 flex items-center justify-between bg-gray-300 mb-3">
+                className="z-20 sticky top-0 px-2 grid grid-cols-3 items-center bg-gray-300 mb-3">
                 <div className="flex">
                     <div
                         className="rounded-t-md relative bg-white mt-1 pl-3 pr-14">
                         <div
                             className={"group flex rounded-md items-center text-sm leading-relaxed py-1 text-black"}
-                            style={{fontFamily: "Arial"}}>
-                            <BsHouseFill className="mr-2"/>
+                            style={{ fontFamily: "Arial" }}>
+                            <BsHouseFill className="mr-2" />
                             Home
                         </div>
                     </div>
@@ -41,9 +41,9 @@ const Navbar = () => {
                             leaveTo="opacity-0"
                         >
 
-                            <Menu.Items className="absolute bg-white w-28 rounded-md mt-1">
+                            <Menu.Items className="absolute bg-white w-28 rounded-md mt-2">
                                 <Menu.Item>
-                                    {({active}) => (
+                                    {({ active }) => (
                                         <div className={`${active && 'bg-gray-300'} block w-full px-2 pt-1 my-1`}>
 
                                             <button
@@ -55,7 +55,7 @@ const Navbar = () => {
                                     )}
                                 </Menu.Item>
                                 <Menu.Item>
-                                    {({active}) => (
+                                    {({ active }) => (
                                         <div className={`${active && 'bg-gray-300'} block w-full px-2 pt-1 my-1`}>
 
                                             <button
@@ -68,7 +68,7 @@ const Navbar = () => {
                                     )}
                                 </Menu.Item>
                                 <Menu.Item>
-                                    {({active}) => (
+                                    {({ active }) => (
                                         <div className={`${active && 'bg-gray-300'} block w-full px-2 pt-1 my-1`}>
 
                                             <button
@@ -85,15 +85,16 @@ const Navbar = () => {
                     </Menu>
 
                 </div>
-                <div className="flex">
-                    <a className="self-center mr-2" href="https://github.com/jeffepok" target="_blank"><BsGithub/></a>
-                    <a className="self-center mr-2" href="https://twitter.com/Tee4Jey" target="_blank"><BsTwitter/></a>
-                    <a className="self-center mr-2" href="https://web.facebook.com/addaipokut" target="_blank"><BsFacebook/></a>
+                <div>
+                    <a href="/"><img className="w-8 mx-auto py-1" src={logo} alt="logo" /></a>
+                </div>
+                <div className="flex flex-row-reverse">
+                    <a className="self-center mr-2" href="https://github.com/jeffepok" rel="noreferrer" target="_blank"><BsGithub /></a>
+                    <a className="self-center mr-2" href="https://twitter.com/Tee4Jey" rel="noreferrer" target="_blank"><BsTwitter /></a>
+                    <a className="self-center mr-2" href="https://web.facebook.com/addaipokut" rel="noreferrer" target="_blank"><BsFacebook /></a>
                 </div>
             </nav>
             <div className="text-white">
-                <a href="/"><img className={"w-14 mx-auto p-1 mb-5"} src={logo} alt="logo"/></a>
-                <hr/>
                 <Typist className="mt-5">
                     <Typist.Delay ms={1000} />
                     JEFFERSON TUFFOUR
